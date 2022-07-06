eb566063ebaabae740e83817581ca376f1124f70
raffrasson <raf.frasson@gmail.com>
2022-07-06 20:44:09 -0300

    chore: initial set-up

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,25 @@
+import { Model, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  username: STRING,
+  role: STRING,
+  email: STRING,
+  password: STRING,
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+export default User;
